bf95f6b410fc97a513735e0f68cdfb38daba749d
Kevin <macchiatoelliott@gmai.com>
2021-03-30 19:48:05 +0800

    basic registration tests

---
+++ src/User.java
@@ -0,0 +1,58 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.UUID;
+
+public class User {
+    private String emailAddress;
+    private String password;
+    private String userName;
+    private String uuid;
+
+    public User(String emailAddress, String password, String userName) {
+        this.emailAddress = emailAddress;
+        this.password = password;
+        this.userName = userName;
+        uuid = UUID.randomUUID().toString();
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public boolean isValidEmailAddress() {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+        Pattern pat = Pattern.compile(emailRegex);
+        return pat.matcher(emailAddress).matches();
+    }
+
+    public boolean isValidPassword() {
+        return password.length()>=8;
+    }
+}
---
+++ test/RegistrationTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+
+public class RegistrationTest {
+
+    private User user;
+
+    @RepeatedTest(10)
+    public void testUuidLength() {
+        user = new User("", "", "");
+        Assert.assertEquals(36,user.getUuid().length());
+    }
+
+    @Test
+    public void testValidEmail() {
+        user = new User("abcd@gmail.com", "123456", "KV");
+        Assert.assertTrue(user.isValidEmailAddress());
+    }
+
+    @Test
+    public void testInvalidEmail() {
+        user = new User("abcd.com", "123456", "KV");
+        Assert.assertFalse(user.isValidEmailAddress());
+    }
+
+    @Test
+    public void testEmptyEmail() {
+        user = new User("", "123456", "KV");
+        Assert.assertFalse(user.isValidEmailAddress());
+    }
+
+    @Test
+    public void testValidPassword() {
+        user = new User("123@gmail.com", "12345678Aa", "KV");
+        Assert.assertTrue(user.isValidEmailAddress());
+    }
+
+    @Test
+    public void testInvalidPassword() {
+        user = new User("123@gmail.com", "abcd", "KV");
+        Assert.assertFalse(user.isValidPassword());
+    }
+
+    @Test
+    public void testEmptyPassword() {
+        user = new User("123@gmail.com", "", "KV");
+        Assert.assertFalse(user.isValidPassword());
+    }
+}
