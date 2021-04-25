deb9dabfe31af36a3c8f4cc1a6e2babb0e544c86
Jintian Wang <jintianw@usc.edu>
2021-04-25 09:47:42 -0700

    cors

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,10 +2,8 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin("*")
 public class Main {
 
 	public static void main(String[] args) {
